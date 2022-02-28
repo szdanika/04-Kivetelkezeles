40fa635e483667c13ab901045111bc22b470f019
szdanika <danikaszab@gmail.com>
2022-02-28 16:25:36 +0100

    Kivetelek

---
+++ 04-Kivetelkezeles/Kivetelek.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Kivetelkezeles
+{
+    internal class KomponensNemTalalhatoKivetelException : Exception
+    {
+        public KomponensNemTalalhatoKivetelException()
+        {
+
+        }
+        public KomponensNemTalalhatoKivetelException(string message) : base(message)
+        {
+
+        }
+    }
+    class NemDeaktivalhatoKivetelException : Exception
+    {
+        public NemDeaktivalhatoKivetelException(string message, Exception e) : base(message,e)
+        {
+
+        }
+    }
+    class KomponensNemFerElKivetelException : Exception
+    {
+        IKomponens komponens;
+        public IKomponens Komponens { get { return komponens; } }
+        public KomponensNemFerElKivetelException(string message, IKomponens komponens) : base(message)
+        {
+            this.komponens = komponens;
+        }
+    }
+    class NincsElegEnergiaKivetelException : Exception
+    {
+        int hianyMerteke;
+        public int HianyMerteke { get { return hianyMerteke; } }
+        public NincsElegEnergiaKivetelException(int hianymerteke) : base("Nincs eleg teljesitmeny," + hianymerteke + "MW hianyzik")
+        {
+            this.hianyMerteke = hianymerteke;
+        }
+    }
+}
